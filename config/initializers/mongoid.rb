a0593ba4b5c14b133d6a6f2b118c31468ab9a1e5
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-10-11 13:44:20 +0200

    removed passenger pre fork stuff from mongoid initializer. Mongoid takes care of this stuff automatically

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,11 +1,3 @@
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    if forked
-      Mongoid.master.connection.connect_to_master
-    end
-  end
-end
-
 Mongoid.database.eval(
   <<-JAVASCRIPT
   db.system.js.save( { _id : 'contains', value : function( array, value ) {
