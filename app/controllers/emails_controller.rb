527a26688e6287c6d6bf57e86493dfc5b80c7c82
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-10-22 16:53:58 +0200

    Revert "more attempts to see what's going on with emails"
    This reverts commit 8097f6784bd9d22ed3edd00bcfed50e7ac443c0f.

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,7 +3,8 @@ class EmailsController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def create
-    Comment.create! :user => User.first, :commentable => Lead.first, :text => params[:email][:raw]
+    Rails.logger.info request.headers.inspect
+    Rails.logger.info params[:email].inspect
     if request.headers['Authorization'] != '20015510-959d-012d-a4ae-001c25a0b06f'
       return head(:unauthorized)
     else
