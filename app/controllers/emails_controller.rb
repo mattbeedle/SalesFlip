9ee4878d8310f55d365e7c2f9861a03676cd88c8
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-10-22 15:25:15 +0200

    Emails controller does not authenticate user

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,6 @@
 class EmailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  skip_before_filter :authenticate_user!
 
   def create
     if request.headers['Authorization'] != '20015510-959d-012d-a4ae-001c25a0b06f'
