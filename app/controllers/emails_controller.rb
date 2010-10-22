8097f6784bd9d22ed3edd00bcfed50e7ac443c0f
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-10-22 16:37:48 +0200

    more attempts to see what's going on with emails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,8 +3,7 @@ class EmailsController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def create
-    Rails.logger.info request.headers.inspect
-    Rails.logger.info params[:email].inspect
+    Comment.create! :user => User.first, :commentable => Lead.first, :text => params[:email][:raw]
     if request.headers['Authorization'] != '20015510-959d-012d-a4ae-001c25a0b06f'
       return head(:unauthorized)
     else
