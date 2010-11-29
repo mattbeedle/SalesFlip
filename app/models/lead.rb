67d07b52ea289648fd98c9082e712ead01a26530
Steven Garcia <stevendgarcia@gmail.com>
2010-11-29 17:56:14 +0100

    Comments now preserved for leads on conversion

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -117,6 +117,7 @@ class Lead
         I18n.locale_around(:en) { update_attributes :status => 'Converted', :contact_id => contact.id }
       end
     end
+    contact.comments << self.comments
     return account || contact.account, contact
   end
 
