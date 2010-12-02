27f91b7afc33dfa8ea1909ffdaa817ef3c58c5f4
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-12-02 14:25:42 +0100

    Hacky fix for carrierwave config after something in gem config messed with mongoid.config.master.connection

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,13 @@ rescue
   raise IOError, 'config/mongoid.yml could not be loaded'
 end
 
+# TODO: since gem updating Mongoid.config.master.connection.host and port don't work
 CarrierWave.configure do |config|
   config.storage              = :grid_fs
   config.grid_fs_database     = Mongoid.database.name
   config.grid_fs_access_url   = '/uploads'
-  config.grid_fs_host         = Mongoid.config.connection.host
-  config.grid_fs_port         = Mongoid.config.connection.port
-  config.grid_fs_username     = Mongoid.config.connection.auths[0]['username']
-  config.grid_fs_password     = Mongoid.config.connection.auths[0]['password']
+  config.grid_fs_host         = Mongoid.config.master.connection.nodes.first.first
+  config.grid_fs_port         = Mongoid.config.master.connection.nodes.first.last
+  config.grid_fs_username     = Mongoid.config.master.connection.auths[0].try(:"['username']")
+  config.grid_fs_password     = Mongoid.config.master.connection.auths[0].try(:"['password']")
 end
