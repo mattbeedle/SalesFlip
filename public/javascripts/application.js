2a2a47d860630aa01ffe0d8f46f960bef8a5bf68
Steven Garcia <stevendgarcia@gmail.com>
2010-12-16 12:14:51 +0100

    truncated comments and fading flash messages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,6 +5,29 @@ var Base = new Class({
     this.addRealTaskCalendar();
     this.hideActivityBodies();
     this.growTextAreas();
+    this.truncateMessages();
+    this.fadeFlashMessages();
+  },
+  
+  fadeFlashMessages: function() {
+    function fadeOut(elem) { new Fx.Fade(elem,{duration:'long'}).start('out');}
+    if ($('flashes') != null) {
+      $('flashes').addClass('fade')
+      $$('.fade').each(function(div) {
+        fadeOut.delay(3500, div);  
+      });
+    }
+  },
+  
+  truncateMessages: function() {
+    $$('div.message div.text').each(function(div) {
+      var size = div.text().length;
+      if (size > 200) {
+        div.addClass('truncated');
+        var link = "<a href='#' class='see_more'>See More</a>";
+        div.parent().insert(link);
+      }
+    });
   },
   
   growTextAreas: function() {
@@ -20,6 +43,10 @@ var Base = new Class({
         resizeMirror(this,mirror);
       }); 
     });
+    function resizeMirror(elem, mirror) {
+      elem.setHeight(mirror.getStyle('height').replace(/px/, '').toInt() + 40);
+      mirror.setWidth(elem.getStyle('width').replace(/px/, '').toInt());
+    };
   },
     
   hideActivityBodies: function() {
@@ -37,7 +64,6 @@ var Base = new Class({
       var abbr      = value_div.get('required')=='true' ? "<abbr>*</abbr>" : '';
       var input     = '<div class="string"><label for="' + id + '">' + abbr + value_div.get('title') + '</label><input id="' + id + '" type="text" name="' + name + '" value="' + value + '"/ autocomplete="off"></div>';
       var date_or_time = value_div.get('format')=='Date' ? "%Y-%m-%d" : "%Y-%m-%d %H:%M"; 
-      
       realdate.find('span').first().remove();
       realdate.insert(input, 'top');
       new Calendar({ format: date_or_time}).assignTo(id);
@@ -45,13 +71,8 @@ var Base = new Class({
   },
 
   watchTitleTogglers: function() {
-
-    //$$("div.toggle").each(function(div) { div.hide(); });
-
     $$("h3.toggler").each(function(h3) {
-
       h3.insert(new Element('span'), 'top');
-
       if ( h3.hasClass('open') ) {
         h3.select('span')[0].update('&#9660;');
         h3.next('.toggle').show();
@@ -59,7 +80,6 @@ var Base = new Class({
       else {
         h3.select('span')[0].update('&#9654;');
       };
-
       h3.onClick(function() {
         if ( this.hasClass('open') ) {
           this.select('span')[0].update('&#9654;');
@@ -87,7 +107,14 @@ function text2html(string) {
   return string.replace(/&/g,'&amp;').replace(/  /g, '&nbsp;').replace(/<|>/g, '&gt;').replace(/\n\r?/g, '<br />');
 };
 
-function resizeMirror(elem, mirror) {
-  elem.setHeight(mirror.getStyle('height').replace(/px/, '').toInt() + 40);
-  mirror.setWidth(elem.getStyle('width').replace(/px/, '').toInt());
-};
+'a.see_more'.on('click', function(event) {
+  if (this.hasClass('open')) {
+    this.parent().find('.text').first().addClass('truncated');
+    this.update('See More').removeClass('open');  
+  }
+  else {
+    this.parent().find('.text').first().removeClass('truncated');
+    this.update('See Less').addClass('open');
+  }
+  event.stop();
+});
