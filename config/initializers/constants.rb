2b8dba9e60833afc5a8bf0c86609536fb944ded3
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-12-19 17:41:47 +0100

    forgot constants file needed to add the new 'Key Account Manager' role

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
 TOP_COUNTRIES = [:DE, :AT, :CH, :GB, :ES, :FR, :US]
 
-ROLES = ['Administrator', 'Sales Person', 'Service Person', 'Freelancer']
+ROLES = ['Administrator', 'Sales Person', 'Service Person', 'Freelancer', 'Key Account Manager']
