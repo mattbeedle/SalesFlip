7e03cc5dc9629665946716b4386d194b79ac7387
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-12-21 13:37:21 +0100

    Fragement caching on index and tasks pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,24 @@ class PagesController < ApplicationController
   before_filter :find_activities, :only => [ :index ]
   before_filter :find_tasks,      :only => [ :index ]
 
+  helper_method :dashboard_cache_key
+
 protected
+  def dashboard_cache_key
+    "dashboard-#{current_user.id.to_s}-#{Activity.action_is_not('Viewed').count}"
+  end
+
   def find_activities
-    @activities ||= Activity.action_is_not('Viewed').desc(:created_at).limit(20).
-      visible_to(current_user)
+    unless read_fragment(dashboard_cache_key)
+      @activities ||= Activity.action_is_not('Viewed').desc(:created_at).limit(20).
+        visible_to(current_user)
+    end
   end
 
   def find_tasks
-    @overdue ||= Task.for(current_user).incomplete.overdue.desc(:due_at)
-    @due_today ||= Task.for(current_user).incomplete.due_today.desc(:due_at)
+    unless read_fragment(dashboard_cache_key)
+      @overdue ||= Task.for(current_user).incomplete.overdue.desc(:due_at)
+      @due_today ||= Task.for(current_user).incomplete.due_today.desc(:due_at)
+    end
   end
 end
