fb11fa4e05dabb133022e254d104bf40b074bffa
Matt Beedle <matt.beedle@1000jobboersen.de>
2011-01-03 15:58:07 +0100

    Decreased flash fade out time

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -34,7 +34,7 @@ var Base = new Class({
     if ($('flashes') != null) {
       $('flashes').addClass('fade')
       $$('.fade').each(function(div) {
-        fadeOut.delay(3500, div);
+        fadeOut.delay(1500, div);
       });
     }
   },
