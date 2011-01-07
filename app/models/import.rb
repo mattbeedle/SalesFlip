2331648470b46b0e5956d4dc26b2856f8118d16f
Matt Beedle <matt.beedle@1000jobboersen.de>
2011-01-07 12:34:28 +0100

    Importing now no longer sends an email to the assignee for every single lead that it imports

--- app/models/import.rb
+++ app/models/import.rb
@@ -47,7 +47,7 @@ protected
 
   def build_attributes(values)
     attributes = { :user => user, :source => 'Imported', :assignee => assignee,
-                   :do_not_index => true }
+                   :do_not_index => true, :do_not_notify => true }
     @fields.each_with_index do |field, i|
       attributes.merge!(field.to_sym => values[i])
     end
