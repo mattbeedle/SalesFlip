925acfb2dbcb1e1a36bab33a07529bf4cac118fa
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-01-13 18:10:46 +0100

    Generate users and leads in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+require Rails.root + 'test/blueprints'
+
+DataMapper.auto_migrate!
+
+puts "Creating users"
+matt = User.make(:matt)
+sales_person = User.make
+
+users = 52.times.map { User.make(company: sales_person.company) }
+
+puts "Creating leads"
+50.times do
+  Lead.make(
+    user: sales_person,
+    permission: 'Private'
+  )
+end
+
+50.times do
+  Lead.make(
+    user: users.sample,
+    permission: 'Public'
+  )
+end
+
+100.times do
+  Lead.make(
+    user: users.sample,
+    assignee: sales_person
+  )
+end
+
+100.times do
+  Lead.make(
+    user: users.sample,
+    permission: 'Shared',
+    permitted_users: [sales_person]
+  )
+end
+
+puts "", "", "", "="*80
+puts "  %20s    %s" % ["admin", matt.email]
+puts "  %20s    %s" % ["sales person", sales_person.email]
+puts "="*80
