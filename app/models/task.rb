c71f8ea8c3fb57010166e1f4774f176fc285f8e3
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-01-19 11:14:55 +0100

    Make task name non-lazy

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,7 +8,7 @@ class Task
   include ActiveModel::Observing
 
   property :id, Serial
-  property :name, Text, :required => true
+  property :name, Text, :required => true, :lazy => false
   property :due_at, Time, :required => true
   property :priority, Integer
   property :completed_at, Time
