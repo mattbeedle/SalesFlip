ebfc1f594a9af45f42d7d63d38dd8a3b67429f7d
Matt Beedle <matt.beedle@1000jobboersen.de>
2011-01-19 21:21:48 +0100

    Carrierwave settings for staging and production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,15 +6,22 @@ end
 
 CarrierWave.configure do |config|
   config.storage              = :grid_fs
-  config.grid_fs_database     = db_config[Rails.env]['database']
   config.grid_fs_access_url   = '/uploads'
-  config.grid_fs_host         = db_config[Rails.env]['host']
-  config.grid_fs_port         = db_config[Rails.env]['port']
   if Rails.env.staging?
+    config.grid_fs_database     = 'salesflip_staging'
+    config.grid_fs_host         = ENV['MONGODB_STAGING_HOST']
     config.grid_fs_username     = ENV['MONGODB_STAGING_USER']
     config.grid_fs_password     = ENV['MONGODB_STAGING_PASSWORD']
+    config.grid_fs_port         = 27017
   elsif Rails.env.production?
+    config.grid_fs_database     = 'salesflip'
+    config.grid_fs_host         = ENV['MONGODB_HOST']
     config.grid_fs_username     = ENV['MONGODB_USER']
     config.grid_fs_password     = ENV['MONGODB_PASSWORD']
+    config.grid_fs_host         = 27017
+  else
+    config.grid_fs_database     = 'salesflip_development'
+    config.grid_fs_host         = 'localhost'
+    config.grid_fs_port         = 27017
   end
 end
