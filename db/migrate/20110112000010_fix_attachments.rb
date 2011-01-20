f875e559d60c262d709d41b50b3d0198f2ddff78
Matt Beedle <matt.beedle@1000jobboersen.de>
2011-01-20 13:18:29 +0100

    Finally think I have the attachment migration working

--- db/migrate/20110112000010_fix_attachments.rb
+++ db/migrate/20110112000010_fix_attachments.rb
@@ -1,12 +1,14 @@
 class FixAttachments < Migrations::MongodbToPostgresql
   def self.up
-    Attachment.all.each do |attachment|
+    Attachment.where(:attachment_filename => nil).each &:destroy
+    Attachment.where(:attachment_filename.not => nil).each do |attachment|
       begin
-        file = CarrierWave::Storage::GridFS::File.new(AttachmentUploader, attachment.attachment.store_path(attachment.attachment_filename))
-        f = File.open(attachment.attachment_filename, 'w+') do |the_file|
-          the_file.write file.read.force_encoding('iso-8859-1')
+        File.open(attachment.attachment_filename, 'wb+') do |file|
+          file.write grid.open(attachment.attachment.store_path(
+            attachment.attachment_filename), 'r').read
         end
-        a = Attachment.new :subject => attachment.subject, :attachment => File.open(attachment.attachment_filename)
+        a = Attachment.new :subject => attachment.subject,
+          :attachment => File.open(attachment.attachment_filename)
         a.save
         a = Attachment.find(a.id)
         a.created_at = attachment.created_at
@@ -14,17 +16,28 @@ class FixAttachments < Migrations::MongodbToPostgresql
         a.updated_at = attachment.updated_at
         a.updated_on = attachment.updated_at ? Date.parse(attachment.updated_at.to_s) : nil
         a.save!
+        attachment.destroy
+        FileUtils.rm(attachment.attachment_filename) if File.exists?(attachment.attachment_filename)
       rescue StandardError => e
         puts e
         puts attachment.legacy_id
-      ensure
-        begin
-          FileUtils.rm(attachment.attachment_filename) if attachment.attachment_filename
-          attachment.destroy
-        rescue
-        end
+        attachment.destroy
       end
     end
+  end
+
+  def self.grid
+    if Rails.env.staging?
+      db ||= Mongo::Connection.new(ENV['MONGODB_HOST'], 27017).db(database)
+      db.authenticate(ENV['MONGODB_STAGING_USER'], ENV['MONGODB_STAGING_PASSWORD'])
+    elsif Rails.env.production?
+      db ||= Mongo::Connection.new(ENV['MONGODB_HOST'], 27017).db(database)
+      db.authenticate(ENV['MONGODB_USER'], ENV['MONGODB_PASSWORD'])
+    else
+      db = Mongo::Connection.new.db(database)
+    end
+    db = Mongo::Connection.new.db('salesflip_development')
+    grid = Mongo::GridFileSystem.new(db)
   end
 
   def self.down
