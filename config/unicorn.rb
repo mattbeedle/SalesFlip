d3d3672df4d107c724f98fa43df386298cc4bd32
Durran Jordan <durran@gmail.com>
2011-01-23 15:41:53 +0100

    Needed working directory configured

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,6 @@
 rails_env = ENV['RAILS_ENV'] || 'production'
-worker_processes 6
+worker_processes 8
+working_directory '/data/salesflip/current'
 preload_app true
 timeout 30
 listen '/data/salesflip/current/tmp/sockets/unicorn.sock', :backlog => 2048
