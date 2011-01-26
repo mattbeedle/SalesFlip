2733c67d935fe4096bcf665e032bbf2cab28af16
Durran Jordan <durran@gmail.com>
2011-01-26 14:03:35 +0100

    Updating unicorn config for hot deploys

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,3 +4,13 @@ working_directory '/data/salesflip/current'
 preload_app true
 timeout 30
 listen '/tmp/salesflip.sock', :backlog => 2048
+
+before_fork do |server, worker|
+  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
