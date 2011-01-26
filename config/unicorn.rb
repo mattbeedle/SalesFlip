eef126f70fa76fa6ed4c3e71992074178673c8e5
Durran Jordan <durran@gmail.com>
2011-01-26 14:42:34 +0100

    Updating unicorn for hot deploys

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ timeout 30
 listen '/tmp/salesflip.sock', :backlog => 2048
 
 before_fork do |server, worker|
-  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  old_pid = "#{Rails.root}/tmp/pids/unicorn.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
