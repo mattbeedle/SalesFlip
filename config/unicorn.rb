b0d3b2ea65625ea4e64caf6f7bf7be8c2973f8f4
Matt Beedle <mattbeedle@googlemail.com>
2011-02-01 08:36:27 +0100

    Ups, pasting code from blogs without checking it == bad. Fixed syntax error

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,5 +17,5 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-  DataObjects::Pooling.pools.each do { |pool| pool.dispose }
+  DataObjects::Pooling.pools.each { |pool| pool.dispose }
 end
