21f0fdbc7767b4dfe3c04d45983f2805c20a3a3a
Durran Jordan <durran@gmail.com>
2011-02-16 11:21:52 +0100

    Updating backups for mongodb and new server

--- app/models/backup_job.rb
+++ app/models/backup_job.rb
@@ -7,31 +7,49 @@ class BackupJob
   class << self
 
     def perform
-      Net::SSH.start("78.47.226.213", "root") do |ssh|
-        ssh.exec("tar -cf ~/main.tar /var/lib/postgresql/9.0/main")
-      end
-      Net::SCP.download!("78.47.226.213", "root", "main.tar", "main.tar")
-      upload_backup
-      delete_backup
+      backup_postgres
+      backup_mongo
+      upload_backups
+      delete_backups
     end
 
     private
 
+    def backup_postgres
+      Net::SSH.start("178.63.20.76", "root") do |ssh|
+        ssh.exec("tar -cf ~/postgres.tar /var/lib/postgresql/9.0/main")
+      end
+      Net::SCP.download!("178.63.20.76", "root", "postgres.tar", "postgres.tar")
+    end
+
+    def backup_mongo
+      Net::SSH.start("46.4.62.14", "root") do |ssh|
+        ssh.exec("tar -cf ~/mongodb.tar /var/lib/postgresql/9.0/main")
+      end
+      Net::SCP.download!("178.63.20.76", "root", "mongodb.tar", "mongodb.tar")
+    end
+
     def bucket
-      AWS.directories.create(
+      @bucket ||= Fog::Storage.new(provider: "AWS").directories.create(
         key: "salesflip-backup-#{Time.now.to_i}",
         public: false
       )
     end
 
-    def delete_backup
-      File.delete("main.tar")
+    def delete_backups
+      File.delete("postgres.tar")
+      File.delete("mongodb.tar")
     end
 
-    def upload_backup
+    def upload_backups
       bucket.files.create(
-        key: "main.tar",
-        body: File.open("main.tar"),
+        key: "postgres.tar",
+        body: File.open("postgres.tar"),
+        public: false
+      )
+      bucket.files.create(
+        key: "mongodb.tar",
+        body: File.open("mongodb.tar"),
         public: false
       )
     end
