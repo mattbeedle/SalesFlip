431fb0194896d070f81a5a72094f7d40a0b46533
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-02-16 17:58:42 +0100

    Special case Rack plugin in non-local environments

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,5 +1,10 @@
 Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'public/stylesheets/compiled' }
 
 Salesflip::Application.configure do
-  config.middleware.insert_before "ActionDispatch::Static", Sass::Plugin::Rack
+  env = Rails.env
+  if env.development? || env.test?
+    config.middleware.insert_before "ActionDispatch::Static", Sass::Plugin::Rack
+  else
+    config.middleware.insert_before "Rack::Lock", Sass::Plugin::Rack
+  end
 end
