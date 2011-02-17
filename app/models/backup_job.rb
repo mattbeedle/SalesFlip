22bfd8cc11300b4292c9022e8a91cb48a7a8679c
Durran Jordan <durran@gmail.com>
2011-02-17 13:08:07 +0100

    Gzip the tarballs in backup

--- app/models/backup_job.rb
+++ app/models/backup_job.rb
@@ -18,15 +18,23 @@ class BackupJob
     def backup_postgres
       Net::SSH.start("178.63.20.76", "root") do |ssh|
         ssh.exec("tar -cf ~/postgres.tar /var/lib/postgresql/9.0/main")
+        ssh.exec("gzip -9 ~/postgres.tar")
       end
-      Net::SCP.download!("178.63.20.76", "root", "postgres.tar", "postgres.tar")
+      Net::SCP.download!("178.63.20.76", "root", "postgres.tar.gz", "postgres.tar.gz")
+      Net::SSH.start("178.63.20.76", "root") do |ssh|
+        ssh.exec("rm ~/postgres.tar.gz")
+      end
     end
 
     def backup_mongo
       Net::SSH.start("46.4.62.14", "root") do |ssh|
         ssh.exec("tar -cf ~/mongodb.tar /data/db/salesflip.*")
+        ssh.exec("gzip -9 ~/mongodb.tar")
       end
-      Net::SCP.download!("46.4.62.14", "root", "mongodb.tar", "mongodb.tar")
+      Net::SCP.download!("46.4.62.14", "root", "mongodb.tar.gz", "mongodb.tar.gz")
+      Net::SSH.start("46.4.62.14", "root") do |ssh|
+        ssh.exec("rm ~/mongodb.tar.gz")
+      end
     end
 
     def bucket
@@ -37,19 +45,19 @@ class BackupJob
     end
 
     def delete_backups
-      File.delete("postgres.tar")
-      File.delete("mongodb.tar")
+      File.delete("postgres.tar.gz")
+      File.delete("mongodb.tar.gz")
     end
 
     def upload_backups
       bucket.files.create(
         key: "postgres.tar",
-        body: File.open("postgres.tar"),
+        body: File.open("postgres.tar.gz"),
         public: false
       )
       bucket.files.create(
         key: "mongodb.tar",
-        body: File.open("mongodb.tar"),
+        body: File.open("mongodb.tar.gz"),
         public: false
       )
     end
