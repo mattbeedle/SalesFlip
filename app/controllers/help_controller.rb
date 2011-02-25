ffc07f53e66cc57f8b27985c57b03f24736ea9b7
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-02-25 11:47:48 +0100

    Localize help documentations

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,3 +1,11 @@
 class HelpController < ApplicationController
   layout 'help'
+
+  around_filter :set_locale
+
+  def set_locale
+    I18n.with_locale(params[:locale]) do
+      yield
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Salesflip::Application.routes.draw do
 
   match 'profile', :to => 'users#profile'
 
-  match 'help(/:action)', :to => "help"
+  match 'help(/:action)(/:locale)', :to => "help"
 
   resources :users, :comments, :tasks, :deleted_items,
     :searches, :invitations, :emails, :opportunities, :campaigns
