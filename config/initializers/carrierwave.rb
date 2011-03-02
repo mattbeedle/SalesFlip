6eac23d97709f8d3d9d97e5c93629ed9b75f9a8f
Matt Beedle <mattbeedle@googlemail.com>
2011-03-02 10:41:41 +0100

    use the same gridfs settings in staging environment (except for database name)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ CarrierWave.configure do |config|
   config.grid_fs_access_url   = '/uploads'
   if Rails.env.staging?
     config.grid_fs_database     = 'salesflip_staging'
-    config.grid_fs_host         = ENV['MONGODB_STAGING_HOST']
-    config.grid_fs_username     = ENV['MONGODB_STAGING_USER']
-    config.grid_fs_password     = ENV['MONGODB_STAGING_PASSWORD']
+    config.grid_fs_host         = ENV['MONGODB_HOST']
+    config.grid_fs_username     = ENV['MONGODB_USER']
+    config.grid_fs_password     = ENV['MONGODB_PASSWORD']
     config.grid_fs_port         = 27017
   elsif Rails.env.production?
     config.grid_fs_database     = 'salesflip'
