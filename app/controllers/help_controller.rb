c80b71c3999bb3dd48e9c4b49d561e84a321904c
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-03-03 13:59:04 +0100

    Don't require authentication for help

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -2,6 +2,7 @@ class HelpController < ApplicationController
   layout 'help'
 
   around_filter :set_locale
+  skip_before_filter :authenticate_user!
 
   def set_locale
     I18n.with_locale(params[:locale]) do
