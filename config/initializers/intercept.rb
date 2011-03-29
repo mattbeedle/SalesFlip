5d9d33772975ec8892e89587dc6de330971fa81e
Bernerd Schaefer <bj.schaefer@gmail.com>
2011-03-29 12:20:29 +0200

    Fix infomail and intercept initializers

--- config/initializers/intercept.rb
+++ config/initializers/intercept.rb
@@ -1,24 +1,20 @@
 if Rails.env.staging?
-  Salesflip::Application.configure do
-    # Intercept all emails and send them to the user who initiated the action.
-    config.after_initialize do
-      class Interceptor
-        cattr_accessor :to
+  # Intercept all emails and send them to the user who initiated the action.
+  class Interceptor
+    cattr_accessor :to
 
-        def self.delivering_email(mail)
-          if to
-            mail.to = to
-            mail.cc = nil
-            mail.bcc = nil
-          end
-        end
+    def self.delivering_email(mail)
+      if to
+        mail.to = to
+        mail.cc = nil
+        mail.bcc = nil
       end
-
-      ApplicationController.before_filter do
-        Interceptor.to = current_user.try(:email)
-      end
-
-      ActionMailer::Base.register_interceptor(Interceptor)
     end
   end
+
+  ApplicationController.before_filter do
+    Interceptor.to = current_user.try(:email)
+  end
+
+  ActionMailer::Base.register_interceptor(Interceptor)
 end
