4b295929e7c670430eb1318a29dfa7a34cc7ddc8
Matt Beedle <mattbeedle@googlemail.com>
2011-04-20 08:51:14 +0200

    Fixed admin index

--- app/controllers/administration/pages_controller.rb
+++ app/controllers/administration/pages_controller.rb
@@ -1,7 +1,2 @@
 class Administration::PagesController < Administration::AdministrationController
-
-  def index
-    @opportunity_stages = current_user.company.opportunity_stages.asc(:percentage).
-      not_deleted
-  end
 end
